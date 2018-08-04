ab8a3ce5d68ed4500aede1b7749c03d01d1ef11e
Tatiana Kuzovleva <kuzovleva@gmail.com>
2018-08-04 11:18:39 -0700

    First file read and write

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import click
 from skill_storage import FileStorage
 
-skill_storage = FileStorage('temp')
+skill_storage = FileStorage('temp.json')
 
 @click.command()
-@click.option('--name', prompt='Your name',
-              help='The person adding skills.')
-def hello(name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    click.echo('Hello %s!' % name)
+def main():
+    """For testing stuff I guess"""
+    skills = skill_storage.list_skills()
+    click.echo(skill_storage.list_skills())
+
+    skills.append({'test':55})
+    skill_storage._overwrite_skills(skills)
+    click.echo(skill_storage.list_skills())
+
+    new_skill = {"I'm new!": "hi"}
+    skill_storage.add_skill(new_skill)
+    click.echo(skill_storage.list_skills())
 
 if __name__ == '__main__':
-    hello()
+    main()
--- skill_storage.py
+++ skill_storage.py
@@ -1,4 +1,24 @@
+import json
+
 class FileStorage(object):
     """Stores skills to a file"""
     def __init__(self, file_name):
+        # TODO check that file exists
         self.file_name = file_name
+
+    def list_skills(self):
+        # TODO memoize?
+        with open(self.file_name) as f:
+            data = json.load(f)
+        if data is None:
+            data = []
+        return data
+
+    def _overwrite_skills(self, skills):
+        with open(self.file_name, 'w') as f:
+            json.dump(skills, f)
+
+    def add_skill(self, skill):
+        skills = self.list_skills()
+        skills.append(skill)
+        self._overwrite_skills(skills)
